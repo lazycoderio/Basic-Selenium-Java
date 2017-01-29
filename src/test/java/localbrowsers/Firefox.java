ae97eeada93987e741f2df898e34e93211133c61
Andrew Krug <andrew.m.krug@gmail.com>
2017-01-28 19:48:08 -0500

    reformatting firefox test

--- src/test/java/localbrowsers/Firefox.java
+++ src/test/java/localbrowsers/Firefox.java
@@ -23,24 +23,24 @@ public class Firefox {
     public void firefoxSetup() {
         DesiredCapabilities capabilities = DesiredCapabilities.firefox();
         capabilities.setCapability("marionette", true);
-        System.out.println(System.getProperty("os.name"));
-        if(!System.getProperty("os.name").toLowerCase().contains("windows")){
-        FirefoxBinary ff = new FirefoxBinary(new File("/Applications/Firefox.app/Contents/MacOS/firefox-bin"));
-        FirefoxProfile ffp = new FirefoxProfile();
-
-        driver = new FirefoxDriver(ff, ffp, capabilities);}
-        else{
+        // mac requires the binary location to be set for firefox, not sure why at time of update
+        if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+            FirefoxBinary ff = new FirefoxBinary(new File("/Applications/Firefox.app/Contents/MacOS/firefox-bin"));
+            FirefoxProfile ffp = new FirefoxProfile();
+            driver = new FirefoxDriver(ff, ffp, capabilities);
+        } else {
             driver = new FirefoxDriver();
         }
     }
+
     @Test
-    public void test(){
+    public void test() {
         driver.get("http://lazycoder.io/about.html");
         Assert.assertEquals(driver.getTitle(), "Lazy Coder Origins");
     }
 
     @AfterTest
-    public void testTeardown(){
+    public void testTeardown() {
         driver.quit();
     }
 }
