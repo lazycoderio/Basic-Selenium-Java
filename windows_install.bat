1429e1fb04d1815aec83990508eac374e354ad74
Andrew Krug <andrew.m.krug@gmail.com>
2017-03-05 09:26:31 -0500

    Update windows_install.bat
    adding in elevating to admin so that the gettools env is set system wide thus usable

--- windows_install.bat
+++ windows_install.bat
@@ -1,7 +1,22 @@
-@echo off
-title Setting up your machine for Selenium Testing!
+     @echo off
+     title Setting up your machine for Selenium Testing!
+     call :isAdmin
 
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+     if %errorlevel% == 0 (
+        goto :run
+     ) else (
+        echo Requesting administrative privileges...
+        goto :UACPrompt
+     )
+
+     exit /b
+
+     :isAdmin
+        fsutil dirty query %systemdrive% >nul
+     exit /b
+
+     :run
+      @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 choco install firefox -y
 choco install googlechrome -y
@@ -10,3 +25,14 @@ choco install selenium-all-drivers -y
 choco install jdk8 -y
 choco install maven -y
 SETX PATH "%PATH%;%ChocolateyToolsLocation%"
+
+     exit /b
+
+     :UACPrompt
+       echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+       echo UAC.ShellExecute "cmd.exe", "/c %~s0 %~1", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+       "%temp%\getadmin.vbs"
+       del "%temp%\getadmin.vbs"
+      exit /B`
+     
